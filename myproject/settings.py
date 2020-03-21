9607265598ed23c357030839bc3a65fb55a551e1
namita <roynamita253@gmail.com>
2020-03-21 15:25:20 +0530

    Intergrated authentication

--- myproject/settings.py
+++ myproject/settings.py
@@ -116,7 +116,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
